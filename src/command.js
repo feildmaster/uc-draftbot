f2bbc6dc6f733dd6b1c5792a646a4338ec5fb434
feildmaster <feildmaster@users.noreply.github.com>
2020-10-02 07:40:39 -0500

    fix: alias, example, and flag filtering

--- src/command.js
+++ src/command.js
@@ -1,17 +1,28 @@
+const flagTemplate = {
+  alias: [''],
+  usage: '',
+  default: '',
+  description: '',
+};
+
 module.exports = class Command {
   constructor({
-    alias = [],
+    title = '',
+    alias = [''],
+    examples = [''],
     usage = '',
     description = '',
-    flags = [],
+    flags = [flagTemplate],
     handler = () => {},
   } = {}) {
-    alias = alias.map((_ = '') => _.trim());
+    alias = alias.filter(_ => _.trim());
     if (!alias.length) throw new Error('No aliases provided.');
+    this.title = title;
     this.alias = alias;
+    this.examples = examples.filter(_ => _.trim());
     this.usage = usage;
     this.description = description;
-    this.flags = flags;
+    this.flags = flags.filter(_ => _.alias.filter(_ => _.trim()).length);
     this.handler = handler;
   }
 };
