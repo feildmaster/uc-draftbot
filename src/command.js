77e557848af4b3894859d4136f6a33f8dbe576c7
feildmaster <feildmaster@users.noreply.github.com>
2020-10-18 12:12:56 -0500

    chore: internally handle command

--- src/command.js
+++ src/command.js
@@ -12,8 +12,9 @@ module.exports = class Command {
     examples = [''],
     usage = '',
     description = '',
+    permission = '',
     flags = [flagTemplate],
-    handler = (context) => context.reply('Missing Handler'),
+    handler = (context, args = [''], flags = {}) => context.reply('Missing Handler'),
   } = {}) {
     alias = alias.filter(_ => _.trim());
     if (!alias.length) throw new Error('No aliases provided.');
@@ -21,8 +22,21 @@ module.exports = class Command {
     this.alias = alias;
     this.examples = examples.filter(_ => _.trim());
     this.usage = usage;
+    this.permission = permission;
     this.description = description;
     this.flags = flags.filter(_ => _.alias.filter(_ => _.trim()).length);
     this.handler = handler;
+  }
+
+  handle(context, ...rest) {
+    if (this.permission) {
+      const permissions = context.channel.permissionsOf(context.user.id);
+      const allowed = Array.isArray(this.permission) ? this.permission.find((perm) => permissions.has(perm)) : permissions.has(this.permission);
+      if (!allowed) {
+        context.reply('Missing permissions');
+        return;
+      }
+    }
+    this.handler(context, ...rest);
   }
 };
